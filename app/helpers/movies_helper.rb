a58724acf073e65b9e3424cc16d99c88e91d8a0d
Shemar Joseph <shemarjoseph@yahoo.com>
2021-05-26 14:48:45 -0400

    Add view helper to determine whether a movie is a flop

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,6 @@
 module MoviesHelper
   def total_gross(movie)
-    if movie.total_gross < 225_000_000
+    if movie.flop?
       "Flop!"
     else 
       number_to_currency(movie.total_gross, precision: 0)
