63680c4f2d2113907430caee7e7ee3491c4e782b
Shemar Joseph <shemarjoseph@yahoo.com>
2021-06-05 18:53:36 -0400

    Add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     format: { with: /\S+@\S/i },
                     uniqueness: true
+  validates :password, length: { minimum: 10, allow_blank: true }
 end
