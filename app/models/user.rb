59b56b63d64ecb78cd8c63281cadb8fc8b382283
Shemar Joseph <shemarjoseph@yahoo.com>
2021-06-05 10:27:56 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true 
+  validates :email, presence: true,
+                    format: { with: /\S+@\S/i },
+                    uniqueness: true
 end
